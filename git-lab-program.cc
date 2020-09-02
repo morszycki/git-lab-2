1f4ca4fd978ef68a627d340daefc3ec7c578d282
Mark Orszycki <mo509316@ohio.edu>
2020-09-02 17:01:00 -0400

    Inital Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mark Orszycki
+ *        Date: 09/02/2020
+ * Description: First program on GitHub
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    /*add code*/
+
+cout << "Hello Git!!" << endl;
+        return 0;
+}// main
